6c45caaf801e310e7709fe7a7d54f05ae028bada
Manuel Prieto Macias <lolop_1993@hotmail.com>
2022-02-22 03:57:17 +0100

    añadido bootstrapn webpack y yarn

---
+++ src/Entity/Asignaturas.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AsignaturasRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AsignaturasRepository::class)]
+class Asignaturas
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $nombre;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+}
